992b727941ed15df12f28cd93bb8de08631d67d9
huxiaoqiao <huxiaoqiao@huxiaoqiaodeMac-mini.local>
2015-01-30 09:45:16 +0800

    Initial Commit

---
+++ CoreGraphicDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreGraphicDemo
+//
+//  Created by 胡晓桥 on 15/1/30.
+//  Copyright (c) 2015年 Qian100. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreGraphicDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreGraphicDemo
+//
+//  Created by 胡晓桥 on 15/1/30.
+//  Copyright (c) 2015年 Qian100. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
